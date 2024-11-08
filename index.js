8a26c18bc0f08dcda2b165a99be04bd5d5fc3938
bobkkblue <chienyec@gmail.com>
2024-11-08 15:16:52 -0800

    updadte

---
+++ index.js
@@ -0,0 +1,10 @@
+//聚会時間
+document.getElementById('date').innerHTML = '日期： 周五(11/08/2024) 晚上七点';
+//聚会地点
+document.getElementById('home').innerHTML = '地点： Paul/Tina家, <a href="https://maps.app.goo.gl/v6BpSCsMR4bpHnuZA">5114 Doreen Ave. Temple  City 91780</a> (可以的话，每家带一道菜)';
+//document.getElementById('home').innerHTML = '地点： 李庆弟兄/云苹姊妹家, <a href="https://maps.app.goo.gl/8KkehPoADML4cGrJ9">4970 Loma Ave. Temple City, 91780</a> (可以的话，每家带一道菜)';
+//聚會詩歌
+document.getElementById('hymn').innerHTML = `诗歌：<a href="https://rosemeaddistrict.github.io/myhymns/%E8%A9%A9%E6%AD%8C/%E5%A4%A7%E6%9C%AC/232%E6%B0%B8%E9%81%A0%E7%9A%84%E6%84%9B%E5%B7%B2%E6%84%9B%E6%88%91.html">大本詩歌232 永遠的愛已愛我</a>`;
+//進度
+document.getElementById('lesson').innerHTML = `进度: 牧养材料96题 第三系列 信而顺服`;
+document.getElementById('class').innerHTML = `<a href="https://e-shepherding.org/%e7%ac%ac%e4%b8%83%e9%a2%98%e3%80%80%e5%ae%9d%e8%b4%9d%e5%9c%a8%e7%93%a6%e5%99%a8%e9%87%8c/">第七题　宝贝在瓦器里</a>`;
