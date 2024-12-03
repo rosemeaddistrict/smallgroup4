52ffd1e83b5fa56b3b034885b831166841185fa4
bobkkblue <chienyec@gmail.com>
2024-12-03 10:43:45 +0800

    updadte

--- index.js
+++ index.js
@@ -143,4 +143,7 @@ async function displayItems(page, element) {
     document.getElementById('class').innerHTML = `<a href=${chapter_link}>${chapter}</a>`;
 }
 
+https://drive.google.com/file/d/1VvHkjo-zEn8C_YN6OitIFK7CzcqKxymq/view?usp=drive_link
+
+1VvHkjo-zEn8C_YN6OitIFK7CzcqKxymq
 
